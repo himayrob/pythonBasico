f0450b426489a3ba01289ae55c022e7c5c8411b7
himayrob <160776152+himayrob@users.noreply.github.com>
2024-02-23 13:26:43 -0500

    aca estoy tratando de darle otro enfoque

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,5 +1,9 @@
 n1=input("escriba el primer numero entero")
 n2=input("escriba el segundo numero entero")
 n3=input("escriba el tercer numero entero")
-if (n1>
-    print(nm)
+
+if int(n1>n2>n3) n1 print("este es el numero mayor" (n1))
+    else
+if int(n2>n1>n3) n2 print("este es el numero mayor" (n2)) 
+    else
+if int(n3>n2>n1) n3 print("este es el numero mayor" (n3 )) 
