a886f956bd44d540351623c942764baf8bbb3882
himayrob <160776152+himayrob@users.noreply.github.com>
2024-02-23 13:13:51 -0500

    aqui estoy tratando de poner le numero mayor como una nueva variable

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,4 +1,5 @@
 n1=input("escriba el primer numero entero")
 n2=input("escriba el segundo numero entero")
-n2=input("escriba el tercer numero entero")
-if n1
+n3=input("escriba el tercer numero entero")
+if (n1>
+    print(nm)
