908783eafd0935a4854eaf10c43a7b3c0f53ec4b
himayrob <160776152+himayrob@users.noreply.github.com>
2024-02-23 14:15:01 -0500

    hay un problema y un no se cual es

--- ejercicio2.py
+++ ejercicio2.py
@@ -2,3 +2,12 @@ n1=input("escriba el primer numero entero")
 n2=input("escriba el segundo numero entero")
 n3=input("escriba el tercer numero entero")
 
+if int (n1 > n2) and int (n1 > n3): 
+print ("este es el numero mayor",(n1))
+elif
+if int (n2 > n1) and int (n2 > n3): 
+print ("este es el numero mayor",(n2))
+elif
+if int (n3 > n1) and int (n3 > n2) 
+print ("este es el numero mayor",(n3))
+else: print ("ninguno es mayor")
