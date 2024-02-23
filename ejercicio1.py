29ff0c04d199ede944d927dd3c09523dda0709ee
himayrob <160776152+himayrob@users.noreply.github.com>
2024-02-23 11:29:15 -0500

    ejercicio uno y parte del dos

---
+++ ejercicio1.py
@@ -0,0 +1,4 @@
+n1=input("dame el primer valor")
+n2=input("dame el segundo Valor")
+suma=int(n1)+int(n2)
+print(suma)
---
+++ ejercicio2.py
@@ -0,0 +1,2 @@
+n1=input("escriba el primer numero entero")
+n2=input("escriba el segundo numero entero")
